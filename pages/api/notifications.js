b5154165d25167fd06110c83fc20c4b2b8b9ed61
beckman924 <braiancarabajal@gmail.com>
2022-07-15 10:59:55 -0300

    Little change to response in notifications

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    req.responseMP = req.body;
+    req.responseMP(req.body);
     res.status(200).send("OK");
   }
   if (req.method === "GET") {
