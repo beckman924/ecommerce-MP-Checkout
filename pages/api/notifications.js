839198ae882201f36942a357a5b91bf90ecd5b91
beckman924 <braiancarabajal@gmail.com>
2022-07-15 16:48:06 -0300

    1

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -1,9 +1,6 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    req.responseMP(req.body);
-    res.status(200).send("OK");
-  }
-  if (req.method === "GET") {
-    res.json(req.responseMP);
+    const responseMP = req.body;
+    res.status(200).json(responseMP);
   }
 }
